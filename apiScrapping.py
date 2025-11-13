88e330b0c99e1e4548af968ef62772fb4ef8d486
alfonsobustamante <athal.zigma@gmail.com>
2025-11-13 15:23:51 -0300

    funcion csvCreator para armar automaticamente un csv a partir de atributos de una pagina.

--- apiScrapping.py
+++ apiScrapping.py
@@ -7,13 +7,30 @@ soup = BeautifulSoup(page_to_scrape.text, "html.parser")
 quotes = soup.findAll("span", attrs={"class":"text"})
 authors = soup.findAll("small", attrs={"class":"author"})
 
+def atrrExtractor(htmlElem, inData):
+    return soup.findAll(htmlElem, attrs={"class":inData})
+
+quotes1 = atrrExtractor("span","text")
+authors1 = atrrExtractor("small","author")
+
 file = open("scrapped.quotes.csv", "w")
 writer = csv.writer(file)
 
-writer.writerow(["Quotes", "Authors"])
+#writer.writerow(["Quotes", "Authors"])
 
-for quote, author in zip(quotes, authors):
-    print(quote.text + " - " + author.text)
-    writer.writerow([quote.text, author.text])
-file.close()
+#for quote, author in zip(quotes, authors):
+#    print(quote.text + " - " + author.text)
+#    writer.writerow([quote.text, author.text])
+#file.close()
 
+def csvCreator(inData1, inData2):
+    attrs1 = atrrExtractor("span",inData1)
+    attrs2 = atrrExtractor("small",inData2)
+    writer.writerow([inData1, inData2])
+
+    for attr1, attr2 in zip(attrs1, attrs2):
+        print(attr1.text + " - " + attr2.text)
+        writer.writerow([attr1.text, attr2.text])
+    file.close()
+
+csvCreator("text", "author")
